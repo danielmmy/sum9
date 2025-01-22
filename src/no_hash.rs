a728d8b1c037f7bc5cb579ae9419d85d26583207
daniel yokoyama <danielmmy10@gmail.com>
2025-01-21 21:50:26 -0300

    refact no hash structure

--- src/no_hash.rs
+++ src/no_hash.rs
@@ -8,21 +8,19 @@ const K: usize = 0x93d765dd;
 const MAX_BYTES: usize = 8;
 
 #[derive(Clone)]
-pub struct NoHash {
-    hash: usize,
-}
+pub struct NoHash(usize);
 
 impl Default for NoHash {
     #[inline]
     fn default() -> Self {
-        NoHash { hash: 0 }
+        NoHash(0)
     }
 }
 
 impl NoHash {
     #[inline]
     fn add_to_hash(&mut self, i: usize) {
-        self.hash = self.hash.wrapping_add(i).wrapping_mul(K);
+        self.0 = self.0.wrapping_add(i).wrapping_mul(K);
     }
 }
 
@@ -76,6 +74,6 @@ impl Hasher for NoHash {
 
     #[inline]
     fn finish(&self) -> u64 {
-        self.hash as u64
+        self.0 as u64
     }
 }
--- src/no_hash_i32.rs
+++ src/no_hash_i32.rs
@@ -5,22 +5,20 @@ const K: usize = 0xf1357aea2e62a9c5;
 #[cfg(target_pointer_width = "32")]
 const K: usize = 0x93d765dd;
 
-#[derive(Debug, Copy, Clone)]
-pub struct NoHashI32 {
-    hash: usize,
-}
+#[derive(Clone)]
+pub struct NoHashI32(usize);
 
 impl Default for NoHashI32 {
     #[inline]
     fn default() -> Self {
-        NoHashI32 { hash: 0 }
+        NoHashI32(0)
     }
 }
 
 impl NoHashI32 {
     #[inline]
     fn add_to_hash(&mut self, i: usize) {
-        self.hash = self.hash.wrapping_add(i).wrapping_mul(K);
+        self.0 = self.0.wrapping_add(i).wrapping_mul(K);
     }
 }
 
@@ -32,7 +30,7 @@ impl Hasher for NoHashI32 {
 
     #[inline]
     fn finish(&self) -> u64 {
-        self.hash as u64
+        self.0 as u64
     }
 
     #[inline]
